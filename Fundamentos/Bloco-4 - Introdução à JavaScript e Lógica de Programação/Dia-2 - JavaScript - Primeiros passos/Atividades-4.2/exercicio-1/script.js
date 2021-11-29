d0bc2386bcd5b238fe212be2c306358e3d70ecba
ThiagoGlady <thiagoglady@gmail.com>
2021-11-29 19:27:06 -0300

    Feitos os exercícios do dia. Falta apenas os três últimos

---
+++ Fundamentos/Bloco-4 - Introdução à JavaScript e Lógica de Programação/Dia-2 - JavaScript - Primeiros passos/Atividades-4.2/exercicio-1/script.js
@@ -0,0 +1,77 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let i;
+let soma = 0;
+let media_aritimetica;
+let maior;
+let checar_impar=0;
+
+//exerício 1
+// for (i=0; i<numbers.length; i += 1) {
+//     console.log(numbers[i]);
+// }
+
+
+
+//exercicio 2
+// for (i=0; i<numbers.length; i +=1) {
+//     soma=soma+numbers[i];
+// }
+
+// console.log(soma);
+
+
+
+// exercicio 3
+// for (i=0; i<numbers.length; i +=1) {
+//     soma=soma+numbers[i];
+// }
+
+// console.log("A soma é: " + soma);
+
+// console.log("Média Arimitética é: " + soma/numbers.length);
+
+
+
+//exercicio 4
+// for (i=0; i<numbers.length; i +=1) {
+//     soma=soma+numbers[i];
+// }
+// media_aritimetica=soma/numbers.length;
+// console.log("A soma é: " + soma);
+
+// console.log("Média Arimitética é: " + media_aritimetica);
+
+// if (media_aritimetica>20) {
+//     console.log("Maior que 20");
+// } else {
+//     console.log("Menor ou igual a 20");
+// }
+
+
+
+//exercicio 5
+// for (i=0; i<numbers.length; i +=1) {
+//     if (numbers[i]==numbers[0]) {
+//         maior=numbers[i];
+//     }
+//     if (numbers[i]>maior) {
+//         maior=numbers[i];
+//     }
+// }
+
+// console.log("O maior valor é: " + maior);
+
+
+
+//exercicio 6
+// for (i=0; i<numbers.length; i +=1) {
+//     if(numbers[i]%2==1){
+//         checar_impar += 1;
+//     }
+// }
+
+// if(checar_impar==0) {
+//     console.log("Não há números ímpares");
+// } else {
+//     console.log("A quantidade de ímpares é: " + checar_impar);
+// }
