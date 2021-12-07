d473562172fd84df69cec7d96b52397f9a1ab508
ThiagoGlady <thiagoglady@gmail.com>
2021-12-07 02:06:29 -0300

    Feito o exercicio 1  5.1

--- Fundamentos/Bloco-5 - JavaScript: DOM, Eventos e Web Storage/Dia-1 - JavaScript - Dom e Seletores/exercicio-1/script.js
+++ Fundamentos/Bloco-5 - JavaScript: DOM, Eventos e Web Storage/Dia-1 - JavaScript - Dom e Seletores/exercicio-1/script.js
@@ -11,5 +11,38 @@ Crie uma função que corrija o texto da tag <h1>.
 Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
+function mudarTexto() {
+    document.getElementsByClassName("center-content")[0].children[1].innerText = "Dois anos de curso";
+}
 
+function mudarCorQuadrado1() {
+    document.getElementsByTagName("main")[0].style.backgroundColor = "rgb(76,164,109)";
+}
+
+function mudarCorQuadrado2() {
+    document.getElementsByClassName("main-content")[0].getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+}
+
+function mudarH1() {
+    document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaEscript";
+}
+
+function deixarMaiusculo () {
+    for(let i = 0; i < document.getElementsByClassName("center-content")[0].children.length; i += 1) {
+        document.getElementsByClassName("center-content")[0].children[i].style.textTransform = "uppercase";
+    }
+}
+
+function mostrarPConsole () {
+    for(let i = 0; i < document.getElementsByClassName("center-content")[0].children.length; i += 1) {
+        console.log(document.getElementsByClassName("center-content")[0].children[i].innerText);
+    }
+}
+
+deixarMaiusculo ();
+mudarH1();
+mudarCorQuadrado2();
+mudarCorQuadrado1();
+mudarTexto ();
+mostrarPConsole ();
 
