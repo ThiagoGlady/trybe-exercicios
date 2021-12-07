e4ccc3e74a6e0b6a3c5d1cac4c7b046d9b1c9282
ThiagoGlady <thiagoglady@gmail.com>
2021-12-07 19:05:19 -0300

    fixação-1 dia 2 bloco 5

---
+++ Fundamentos/Bloco-5 - JavaScript: DOM, Eventos e Web Storage/dia-2 - JavaScript - Trabalhando com elementos/fixacao-1/script.js
@@ -0,0 +1,7 @@
+document.getElementById("elementoOndeVoceEsta").parentElement.style.backgroundColor = "red";
+document.getElementById("elementoOndeVoceEsta").children[0].innerText = "Adicionado Texto";
+document.getElementById("pai").firstElementChild;
+document.getElementById("elementoOndeVoceEsta").previousElementSibling.innerText = "é nóix";
+document.getElementById("elementoOndeVoceEsta").nextSibling;
+document.getElementById("elementoOndeVoceEsta").nextSibling.nextSibling;
+document.getElementById("pai").children[2];
