a229bad12ea487b5b788a0eb13f4b3554f3b9944
ThiagoGlady <thiagoglady@gmail.com>
2021-12-17 23:13:16 -0300

    exercicio 5 bloco 5 dia 3 feito

--- Fundamentos/Bloco-5 - JavaScript: DOM, Eventos e Web Storage/dia-3 - JavaScript - Eventos/exercicio-1/script.js
+++ Fundamentos/Bloco-5 - JavaScript: DOM, Eventos e Web Storage/dia-3 - JavaScript - Eventos/exercicio-1/script.js
@@ -94,3 +94,21 @@ function botaoSextaFeira(sextaFeira) {
 }
 
 botaoSextaFeira("Sexta-Feira");
+
+
+
+// Exercício 5:
+// Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+
+function trocarTexto(botaoSexta) {
+  for(let i = 5; i <= 26; i += 7) {
+    if(document.getElementsByClassName("day")[i].style.fontWeight == "1000") {
+      document.getElementsByClassName("day")[i].style.fontWeight = "500";
+    }else {
+      document.getElementsByClassName("day")[i].style.fontWeight = "1000";
+    }
+  }
+}
+
+document.getElementById("btn-friday").addEventListener("click", trocarTexto);
